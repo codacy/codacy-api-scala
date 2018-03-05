fb0e30ed2994ce82c461f7718b7a3f72821c4dbf
Daniel Reigada <daniel@codacy.com>
2018-03-05 14:24:20 +0000

    Add method to git client to get latest commit info

--- src/main/scala/com/codacy/api/helpers/vcs/GitClient.scala
+++ src/main/scala/com/codacy/api/helpers/vcs/GitClient.scala
@@ -1,12 +1,15 @@
 package com.codacy.api.helpers.vcs
 
 import java.io.File
+import java.util.Date
 
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.lib.{Repository, RepositoryBuilder}
 
 import scala.collection.JavaConverters._
 import scala.util.Try
+
+case class CommitInfo(uuid: String, authorName: String, authorEmail: String, date: Date)
 
 class GitClient(workDirectory: File) {
 
@@ -20,6 +23,18 @@ class GitClient(workDirectory: File) {
         headRev.getName
       }
     }.toOption.flatten.filter(_.trim.nonEmpty)
+  }
+
+  def latestCommitInfo: Option[CommitInfo] = {
+    Try {
+      repository.map { rep =>
+        val git = new Git(rep)
+        val headRev = git.log().setMaxCount(1).call().asScala.head
+        val authorIdent = headRev.getAuthorIdent
+
+        CommitInfo(headRev.getName, authorIdent.getName, authorIdent.getEmailAddress, authorIdent.getWhen)
+      }
+    }.toOption.flatten
   }
 
 }
