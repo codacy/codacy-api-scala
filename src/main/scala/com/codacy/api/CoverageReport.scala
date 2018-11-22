b2ab5ceac7c1c64e396c4ad4d4ea1ea396dc292d
Pedro Amaral <pamaral@codacy.com>
2018-11-22 15:14:20 +0000

    remove empty lines

--- src/main/scala/com/codacy/api/CoverageReport.scala
+++ src/main/scala/com/codacy/api/CoverageReport.scala
@@ -6,10 +6,7 @@ case class CoverageFileReport(filename: String, total: Int, coverage: Map[Int,In
 
 case class CoverageReport(total: Int, fileReports: Seq[CoverageFileReport])
 
-
 object CoverageReport {
-
   implicit val coverageFileReportWrites: Writes[CoverageFileReport] = Json.writes[CoverageFileReport]
   implicit val coverageReportWrites: Writes[CoverageReport] = Json.writes[CoverageReport]
-
 }
--- src/main/scala/com/codacy/api/Result.scala
+++ src/main/scala/com/codacy/api/Result.scala
@@ -7,7 +7,6 @@ case class Result(rule: String, filePath: String, line: Int, message: String)
 case class ResultReport(algorithmUUID: String, commitUUID: String, results: Seq[Result])
 
 object ResultReport {
-
   implicit val resultFormatter: Format[Result] = Json.format[Result]
   implicit val resultReportFormatter: Format[ResultReport] = Json.format[ResultReport]
 }
