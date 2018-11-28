9f89fb63475658e8253f2d5baf78a60fb8bb7e87
Pedro Amaral <pamaral@codacy.com>
2018-11-28 18:52:07 +0000

    add new serializer for coverage reports to match codacy api

--- src/main/scala/com/codacy/api/CoverageReport.scala
+++ src/main/scala/com/codacy/api/CoverageReport.scala
@@ -1,12 +1,17 @@
 package com.codacy.api
 
-import play.api.libs.json.{Json, Writes}
+import play.api.libs.json.{JsNumber, JsObject, Json, Writes}
 
 case class CoverageFileReport(filename: String, total: Int, coverage: Map[Int,Int])
 
 case class CoverageReport(total: Int, fileReports: Seq[CoverageFileReport])
 
 object CoverageReport {
+  implicit val mapWrites: Writes[Map[Int,Int]] = Writes[Map[Int,Int]] { map : Map[Int,Int] =>
+    JsObject(map.map{
+      case (key,value) => (key.toString,JsNumber(value))
+    }(collection.breakOut))
+  }
   implicit val coverageFileReportWrites: Writes[CoverageFileReport] = Json.writes[CoverageFileReport]
   implicit val coverageReportWrites: Writes[CoverageReport] = Json.writes[CoverageReport]
 }
