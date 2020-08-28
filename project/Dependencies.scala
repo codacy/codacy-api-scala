00502fce480b0ebefb4da44a5cbe356a81ccae3d
André Meira <andre.meira@codacy.com>
2020-08-28 14:59:06 +0100

    feature: Improve error handling when parsing API responses [CY-2365]
    This commit is labeled as `feature` since it introduces a dependency
    on `scala-logging`. However, it does not enforce any compatible logging
    backend. That choice is made by whoever uses this library.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
 
-  val playJson = "com.typesafe.play" %% "play-json" % "2.6.10" withSources()
-
+  val playJson = "com.typesafe.play" %% "play-json" % "2.6.10" withSources ()
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 }
--- src/main/scala/com/codacy/api/client/CodacyClient.scala
+++ src/main/scala/com/codacy/api/client/CodacyClient.scala
@@ -2,13 +2,13 @@ package com.codacy.api.client
 
 import play.api.libs.json._
 import com.codacy.api.util.JsonOps
+import com.typesafe.scalalogging.LazyLogging
 import scalaj.http.Http
 
-class CodacyClient(
-    apiUrl: Option[String] = None,
-    apiToken: Option[String] = None,
-    projectToken: Option[String] = None
-) {
+import scala.util.{Failure, Success, Try}
+
+class CodacyClient(apiUrl: Option[String] = None, apiToken: Option[String] = None, projectToken: Option[String] = None)
+    extends LazyLogging {
 
   private case class ErrorJson(error: String)
   private case class PaginatedResult[T](next: Option[String], values: Seq[T])
@@ -103,9 +103,14 @@ class CodacyClient(
   }
 
   private def parseJson(input: String): RequestResponse[JsValue] = {
-    val json = Json.parse(input)
-    json
-      .validate[ErrorJson]
-      .fold(_ => SuccessfulResponse(json), apiError => FailedResponse(s"API Error: ${apiError.error}"))
+    Try(Json.parse(input)) match {
+      case Success(json) =>
+        json
+          .validate[ErrorJson]
+          .fold(_ => SuccessfulResponse(json), apiError => FailedResponse(s"API Error: ${apiError.error}"))
+      case Failure(exception) =>
+        logger.error(s"Failed to parse API response as JSON - $input", exception)
+        FailedResponse("Failed to parse API response as JSON")
+    }
   }
 }
