bde754ec9a1259d5a0ddc26fa294f1e51b72f62b
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2021-01-06 10:57:21 +0100

    fix: Replace spaces with %20 in request URLs :breaking:
    Make class Request not a case class

--- src/main/scala/com/codacy/api/client/Request.scala
+++ src/main/scala/com/codacy/api/client/Request.scala
@@ -1,3 +1,10 @@
 package com.codacy.api.client
 
-case class Request[T](endpoint: String, classType: Class[T], queryParameters: Map[String, String] = Map.empty)
+class Request[T] private (val endpoint: String, val classType: Class[T], val queryParameters: Map[String, String])
+object Request {
+
+  def apply[T](endpoint: String, classType: Class[T], queryParameters: Map[String, String] = Map.empty): Request[T] = {
+    val sanitizedEndpoint = endpoint.replaceAll(" ", "%20")
+    new Request[T](sanitizedEndpoint, classType, queryParameters)
+  }
+}
