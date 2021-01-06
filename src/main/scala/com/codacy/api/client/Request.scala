32d02a2721075199e6da7785b5f763d4f4b683df
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2021-01-06 11:17:55 +0100

    style: Run Scalafmt

--- src/main/scala/com/codacy/api/client/Request.scala
+++ src/main/scala/com/codacy/api/client/Request.scala
@@ -1,6 +1,7 @@
 package com.codacy.api.client
 
 class Request[T] private (val endpoint: String, val classType: Class[T], val queryParameters: Map[String, String])
+
 object Request {
 
   def apply[T](endpoint: String, classType: Class[T], queryParameters: Map[String, String] = Map.empty): Request[T] = {
