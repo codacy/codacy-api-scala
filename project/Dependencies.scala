fbe9a98e0c982f96b69fc58692f4fc63f244d157
Tiago Gonçalves <116074926+tfrgoncalves@users.noreply.github.com>
2024-01-24 14:30:45 +0000

    fix: scalaj dep (#55)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val playJson = "com.typesafe.play" %% "play-json" % "2.8.2"
-  val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.2"
+  val scalajHttp = "com.codacy" %% "scalaj-http" % "2.5.0"
   val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "4.8.0.201706111038-r"
   val jodaConvert = "org.joda" % "joda-convert" % "2.1.2"
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
